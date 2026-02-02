0fddefb426446914076342595199d8d1400df388
abdulrafay <abdulrafayb88@gmail.com>
2026-02-02 19:36:19 +0500

    vercel changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Make sure this is just a forward slash
 })
