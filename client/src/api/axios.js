b241d726cc11d18088bef527e4d41f0d347d10c0
abdulrafay <abdulrafayb88@gmail.com>
2026-02-02 19:04:24 +0500

    file edited

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   // baseURL: '/api'
   baseURL: import.meta.env.VITE_API_URL || '/api',
-  // baseURL: 'http://localhost:5000/api', // Change for production
+  // baseURL: 'http://localhost:5000/api', 
 
 });
 
