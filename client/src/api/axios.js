b3651f6ca1d85166657dd68d883ee6d350b36f52
abdulrafay <abdulrafayb88@gmail.com>
2026-02-11 00:19:25 +0500

    axios.js fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '/api',
+  // baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || "https://stock-sync-one.vercel.app",
   // baseURL: 'http://localhost:5000/api', 
 
 });
