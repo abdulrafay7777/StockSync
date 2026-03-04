183cda24e2879c0cc8818aa47fe1ee7c8be70262
abdulrafay <abdulrafayb88@gmail.com>
2026-03-05 04:27:49 +0500

    Update axios baseURL for Vercel production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: '/api',
+  baseURL: '/api',
   // baseURL: import.meta.env.VITE_API_URL || "https://stock-sync-one.vercel.app",
-  baseURL: 'http://localhost:5000/api', 
+  // baseURL: 'http://localhost:5000/api', 
 
 });
 
